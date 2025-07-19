c74d7df6e6be8f0dc6d1796a72c83e816b795169
morSharabi13 <mor.sharabi@fiverr.com>
2025-07-19 16:44:21 +0300

    handle null url

--- phrases.js
+++ phrases.js
@@ -1,39 +1,36 @@
 const phrases = [
     {
         phrase: "קליפ זיכרון",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        url: "https://www.youtube.com/watch?v=aSKyzKFf57E&ab_channel=MorSharabi"
     },
     {
         phrase: "סרטון חנייה",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        url: "https://youtu.be/9fmOS44raxg?si=OGoTRy1kaSuGyXI3"
     },
     {
         phrase: "כדורגל במרפסת",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     },
     {
         phrase: "שמריז הגיע באוטובוס אלינו לצפון כדי לטייל איתנו",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        url: "https://photos.google.com/share/AF1QipNWslZn_pWNc9NrJmPpmvMPe8Ad_A_XGPnsPDxxooZXbmUaFw5w3AC0vcbnXgfLNw/photo/AF1QipO7Z6posQEYgRsKdoPsRld5GjuivUuXmlJBnNw"
     },
     {
         phrase: "המסיבה בפלמחים",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        url: "https://photos.google.com/share/AF1QipNWslZn_pWNc9NrJmPpmvMPe8Ad_A_XGPnsPDxxooZXbmUaFw5w3AC0vcbnXgfLNw/photo/AF1QipNP0Ox_nGmeJaEx4C0AYvH_-nRMiDpKoX-nSxjr"
     },
     {
         phrase: "מחבט הבייסבול והמנה חמה",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
     },
     {
         phrase: "הפנצרים עם זיוי",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
     },
     {
         phrase: "הדוח",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     },
     {
         phrase: "שמריז מזמין סטייק ב400 שקל",
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     },
 
 ];
