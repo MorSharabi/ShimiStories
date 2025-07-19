7c0233715a3ddd91fafb108069227912d23ec119
morSharabi13 <mor.sharabi@fiverr.com>
2025-07-19 16:06:07 +0300

    add

--- phrases.js
+++ phrases.js
@@ -1,13 +1,41 @@
 const phrases = [
-    "קליפ זיכרון",
-    "סרטון חנייה",
-    "כדורגל במרפסת",
-    "שמריז הגיע באוטובוס אלינו לצפון כדי לטייל איתנו",
-    "המסיבה בפלמחים",
-    "מחבט הבייסבול והמנה חמה",
-    "הפנצרים עם זיוי",
-    "הדוח",
-    "שמריז מזמין סטייק ב400 שקל"
+    {
+        phrase: "קליפ זיכרון",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        phrase: "סרטון חנייה",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        phrase: "כדורגל במרפסת",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        phrase: "שמריז הגיע באוטובוס אלינו לצפון כדי לטייל איתנו",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        phrase: "המסיבה בפלמחים",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        phrase: "מחבט הבייסבול והמנה חמה",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        phrase: "הפנצרים עם זיוי",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        phrase: "הדוח",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+    {
+        phrase: "שמריז מזמין סטייק ב400 שקל",
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    },
+
 ];
 
 
